0b0d58745a945c8b4d4923f454a767771947d5ca
AlanTheKnight <pythonist@tutanota.com>
2020-02-09 11:22:28 +0300

    Deleted 404

--- school_diary/main/urls.py
+++ school_diary/main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404, handler500
 from . import views
 
 
@@ -11,7 +10,3 @@ urlpatterns = [
     path('help/', views.get_help, name='help'),
     path('error/', views.error404, name='404'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-handler404 = views.error404
-handler500 = views.error500
