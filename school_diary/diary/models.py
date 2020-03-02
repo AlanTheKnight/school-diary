bb6cc76215e35851a84b3d7eddc451f6afc43fda
rudolf <rudolfss13@gmail.com>
2020-03-03 01:08:04 +0300

    Speed up save.

--- school_diary/diary/models.py
+++ school_diary/diary/models.py
@@ -177,7 +177,6 @@ class Marks(models.Model):
     lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE, verbose_name='Урок')
     subject = models.ForeignKey(Subjects, on_delete=models.CASCADE, verbose_name='Предмет', null=True, default=None)
     comment = models.TextField(max_length=300, verbose_name="Комментарий", default="", blank=1)
-    date = models.DateField(null=True)
     class Meta:
         verbose_name = "Оценка"
         verbose_name_plural = "Оценки"
