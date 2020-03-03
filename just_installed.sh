159bbba8e478ca632e9f974e99e4e1e5be0319d8
AlanTheKnight <ideasoft-spb@yandex.com>
2020-03-04 01:01:05 +0300

    Update just_installed.sh

--- just_installed.sh
+++ just_installed.sh
@@ -2,5 +2,5 @@ python -m pip install -r requirements.txt
 cd school_diary
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --username administrator --email ideasoft-spb@yandex.com
+python manage.py createsuperuser
 python manage.py runserver
