d6a0e888046f48d6ea1800f02b374946b4bb3551
AlanTheKnight <ideasoft-spb@yandex.com>
2020-03-07 18:22:01 +0300

    Git merge conflict resolved.

--- school_diary/diary/views.py
+++ school_diary/diary/views.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import datetime
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
