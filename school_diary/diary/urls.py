27312ff1c52f6fb94394d7dd02e655c243fc6b40
AlanTheKnight <ideasoft-spb@yandex.com>
2020-03-15 01:00:56 +0300

    Fix

--- school_diary/diary/urls.py
+++ school_diary/diary/urls.py
@@ -75,10 +75,11 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('error/', views.error404, name='404'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     import debug_toolbar
     urlpatterns = [
                       path('__debug__/', include(debug_toolbar.urls)),
--- school_diary/school_diary/settings.py
+++ school_diary/school_diary/settings.py
@@ -103,17 +103,25 @@ WSGI_APPLICATION = 'school_diary.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'diarydb',
-        'USER': 'diaryuser',
-        'PASSWORD': 'Forward#Forever2005',
-        'HOST': 'localhost',
-        'PORT': '',
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'diarydb',
+            'USER': 'diaryuser',
+            'PASSWORD': 'Forward#Forever2005',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ATOMIC_REQUESTS': True,
+        }
+    }
 
 
 # Password validation
