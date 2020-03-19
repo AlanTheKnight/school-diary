a9d964a55d7f8eda04cb0ee1af890451ef16c488
alan <ideasoft-spb@yandex.com>
2020-03-19 10:05:27 +0000

    Some changes

--- school_diary/diary/views.py
+++ school_diary/diary/views.py
@@ -303,7 +303,7 @@ def diary(request):
                 n_amount = 0
                 marks_list = []
                 for i in marks:
-                    if i.lesson.control.name == 'Четвертная' or i.lesson.control.name == 'Годовая': # delete Term or Year mark from avg
+                    if i.lesson.control.name == 'Четвертная оценка ' or i.lesson.control.name == 'Годовая оценка ': # delete Term or Year mark from avg
                         continue
 
                     if i.amount != -1:
@@ -498,6 +498,8 @@ def stats(request, id, term):
         marks_list = []
         for i in marks:
             if i.amount != -1:
+                if i.control.name == 'Годовая оценка' or i.control.name == 'Четвертная оценка':
+                    continue
                 marks_list.append(i)
             else:
                 n_amount += 1
