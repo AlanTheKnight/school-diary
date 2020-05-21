65d2931e254c9f7bc584860f5325d09314e6b601
Pashs-ba <pashs.ba@gmail.com>
2020-05-21 08:48:23 +0300

    remove some functions from views

--- school_diary/diary/views.py
+++ school_diary/diary/views.py
@@ -16,15 +16,7 @@ from .decorators import (
     unauthenticated_user, admin_only, allowed_users,
     teacher_only, student_only)
 from . import models
-from .functions import get_quarter_by_date
-
-
-TERMS = (
-    ((1, 7), (27, 10)),
-    ((3, 11), (29, 12)),
-    ((12, 1), (21, 3)),  # FIX ON PRODUCTION
-    ((29, 3), (27, 5))
-)
+from .functions import *
 
 
 @unauthenticated_user
@@ -150,25 +142,6 @@ def lesson_page(request, pk):
     return render(request, 'lesson_page.html', context)
 
 
-def get_average(list):
-    if len(list) == 0:
-        return '-'
-    grades = sum([i.amount for i in list])
-    return round(grades / len(list), 2), grades, len(list)
-
-
-def get_smart_average(list):
-    if len(list) == 0:
-        return "-"
-    s = 0
-    w = 0
-    for i in list:
-        weight = i.lesson.control.weight
-        s += weight * i.amount
-        w += weight
-    return round(s / w, 2)
-
-
 @login_required(login_url='/login/')
 @allowed_users(allowed_roles=['teachers'], message="Вы не зарегистрированы как учитель.")
 def delete_lesson(request, pk):
@@ -179,104 +152,16 @@ def delete_lesson(request, pk):
     return render(request, 'lesson_delete.html', {'item': lesson})
 
 
-def create_table(grade, subject, quarter):
-    lessons = {
-        lesson.id: lesson for lesson in
-        models.Lessons.objects.filter(grade=grade, subject=subject, quarter=quarter).select_related("control").order_by(
-            "date").all()
-    }
-    students = {student.account_id: student for student in
-                models.Students.objects.filter(grade=grade).order_by("surname", "first_name", "second_name")}
-
-    marks = models.Marks.objects.filter(
-        student__grade_id=grade.id,
-        lesson__grade_id=grade.id,
-        lesson__subject_id=subject.id,
-        lesson__quarter=quarter,
-    )
-
-    scope = {}
-    avg = {}
-    for mark in marks:
-        def increase_avg(mark):
-            if mark.amount != -1 and mark.lesson.control.weight != 100:
-                avg[mark.student_id][0] += mark.amount * mark.lesson.control.weight
-                avg[mark.student_id][1] += mark.lesson.control.weight
-                avg[mark.student_id][2] += mark.amount
-                avg[mark.student_id][3] += 1
-        if students[mark.student_id] not in scope:
-            scope[students[mark.student_id]] = {}
-        lesson = lessons[mark.lesson_id]
-        scope[students[mark.student_id]].update({lesson: mark})
-        if mark.student_id in avg:
-            increase_avg(mark)
-        else:
-            avg[mark.student_id] = [0, 0, 0, 0]
-            increase_avg(mark)
-
-    for sk, student in students.items():
-        for lk, lesson in lessons.items():
-            if student not in scope:
-                scope[student] = {}
-                avg[student.pk] = [0, 0, 0, 0]
-            if lesson not in scope[student]:
-                scope[student].update({lesson: None})
-
-    scope = sorted(list(scope.items()), key=lambda student: student[0].surname)
-
-    return {
-        'is_post': True,
-        'lessons': lessons,
-        'scope': scope,
-        'subject_id': subject.id,
-        'grade_id': grade.id,
-        'avg': avg
-    }
 
 
-def year_valid(controls):
-    '''
-    check date to year mark
-    '''
-    # OPTIMIZATION: The same gist as in term_valid(): use timedelta to know the days
-    year = datetime.date.today().year
-    delta = datetime.date(year, TERMS[3][1][1], TERMS[3][1][0]) - datetime.date.today()
-    if delta.days < 14:
-        return controls
-    else:
-        return controls.exclude(name='Годовая оценка')
 
 
-def term_valid(controls, terms):
-    '''
-    check dates to term marks
-    '''
-    year = datetime.date.today().year
-    a = 0
-    for i in range(1, 5):
-        # FIX OPTIMIZATION:
-        # If delta between today and the end of the quarter lower than 14 days, I allow setting quarter marks.
-        delta = datetime.date(year, terms[i - 1][1][1], terms[i - 1][1][0]) - datetime.date.today()
-        if delta.days < 14:
-            a = 1
-            break
-    if not a:
-        return controls.exclude(name='Четвертная оценка')
-    else:
-        return controls
 
 
-def create_controls(grade, subject, term):
-    controls = models.Controls.objects.all()
-    controls = term_valid(controls, TERMS)
-    controls = year_valid(controls)
-    lessons = models.Lessons.objects.filter(grade=grade, subject=subject, quarter=term).all()
-    for lesson in lessons:
-        if lesson.control.name == 'Четвертная оценка':
-            controls = controls.exclude(name='Четвертная оценка')
-        if lesson.control.name == 'Годовая оценка':
-            controls = controls.exclude(name='Годовая оценка')
-    return {'controls': controls}
+
+
+
+
 
 def students_diary(request):
     student = models.Students.objects.get(account=request.user)
