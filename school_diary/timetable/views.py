7b62b0d1b7dab94f1e2cca005d219684a4e27d98
AlanTheKnight <ideasoft-spb@yandex.com>
2020-05-30 19:34:54 +0300

    Revert "Merge pull request #40 from ideasoft-spb/velutb"
    This reverts commit e3699c511dfc87c790e678f7eaa20e0b934aa060, reversing
    changes made to 330798d70061b195e7e5929a2df19e7ccbc6d28b.

--- school_diary/timetable/views.py
+++ school_diary/timetable/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from urllib.parse import unquote
-from .forms import GetTimeTableForm, LessonCreateForm, BellCreateForm
-from django.http import HttpResponseRedirect, HttpResponse
-from .models import Grades, Lessons, BellsTimeTable
-import time
-import json
-from .decorators import admin_only
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.paginator import Paginator
 from .forms import GetTimeTableForm
 from django.http import HttpResponseRedirect
 from .models import Grades, Lessons
 import time
+
+
 from collections import OrderedDict
 
 
@@ -41,69 +35,35 @@ def timetable(request):
         return render(request, 'timetable/timetable.html', {'form': form})
 
 
-def output(request):
-    form = GetTimeTableForm()
-    return render(request, 'timetable/list.html',{'form': form})
+def output(request, grade, litera):
+    data = {}
+    """Shows the timetable depending on the url."""
+    CURRENT_DAY = time.localtime().tm_wday + 1
+    # If current day isn't sunday, users will see timetable for today.
+    current_day_name = DAYWEEK_NAMES[CURRENT_DAY]
+    # If surrent day isn't friday, users will see timetable for tomorrow.
+    if CURRENT_DAY != 6:
+        next_day_name = DAYWEEK_NAMES[(CURRENT_DAY + 1) % 7]
+    class_number = int(grade)
+    class_letter = litera
+    my_class = str(class_number) + class_letter
+    my_grade = get_object_or_404(Grades, number=class_number, letter=class_letter)
+    all_lessons = Lessons.objects.filter(connection=my_grade.id)
+    if CURRENT_DAY != 7:
+        data["today"] = all_lessons.filter(day=current_day_name)
+    else:
+        data["today"] = []
+    if CURRENT_DAY != 6:
+        data["tomorrow"] = all_lessons.filter(day=next_day_name)
+    else:
+        data["tomorrow"] = []
+    for weekday in DAYWEEK_NAMES.values():
+        data[weekday] = all_lessons.filter(day=weekday)
+    return render(request, 'timetable/list.html', {
+        'current_weekday': current_day_name,
+        'data': data,
+        'my_grade': my_class})
 
 
 def download(request):
     return render(request, 'timetable/download.html')
-
-
-def return_results(chosen_grade, chosen_litera):
-    try:
-        my_grade = Grades.objects.get(number=chosen_grade, letter=chosen_litera)
-    except ObjectDoesNotExist:
-        my_grade = Grades.objects.create(number=chosen_grade, letter=chosen_litera)
-        my_grade.save()
-    lessons = Lessons.objects.filter(connection=my_grade).order_by('day', 'number')
-    return lessons
-
-
-def aj(request):
-    CURRENT_DAY = time.localtime().tm_wday + 1
-    current_day_name = DAYWEEK_NAMES[CURRENT_DAY]
-    next_day_name = DAYWEEK_NAMES[(CURRENT_DAY + 1) % 7]
-    g = request.GET.get("g")
-    l = request.GET.get("l")
-    l = unquote(l)
-    my_grade = Grades.objects.get(number=g, letter=l)
-    all_lessons = Lessons.objects.filter(connection=my_grade.id)
-    week = {}
-    today_lessons = all_lessons.filter(day=current_day_name)
-    tomorrow_lessons = all_lessons.filter(day=next_day_name)
-    today = []
-    for les in today_lessons:
-        today.append({
-            "num": les.number.n,
-            "sub": les.subject,
-            "cls": les.classroom,
-            "strt": str(les.number.start)[:-3],
-            "end": str(les.number.end)[:-3]
-        })
-    week['Сегодня ({0})'.format(current_day_name)] = today
-    tomorrow = []
-    for les in tomorrow_lessons:
-        tomorrow.append({
-            "num": les.number.n,
-            "sub": les.subject,
-            "cls": les.classroom,
-            "strt": str(les.number.start)[:-3],
-            "end": str(les.number.end)[:-3]
-        })
-    week['Завтра ({0})'.format(next_day_name)] = tomorrow
-    DAYWEEK_NAMES_list = list(DAYWEEK_NAMES.values())
-    for weekday in DAYWEEK_NAMES_list[:-1]:
-            week_les = all_lessons.filter(day=weekday)
-            week_list = []
-            for les in week_les:
-                week_list.append({
-                    "num": les.number.n,
-                    "sub": les.subject,
-                    "cls": les.classroom,
-                    "strt": str(les.number.start)[:-3],
-                    "end": str(les.number.end)[:-3]
-                })
-            week[weekday] = week_list
-    week_json = json.dumps(week)
-    return HttpResponse(week_json)
