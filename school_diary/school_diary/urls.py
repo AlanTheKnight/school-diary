d26c49aae5c1936af38182bd6a0742cc534c48d2
AlanTheKnight <ideasoft-spb@yandex.com>
2020-08-17 14:13:49 +0300

    Fix problem with Django version

--- school_diary/school_diary/urls.py
+++ school_diary/school_diary/urls.py
@@ -12,9 +12,5 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('grades/', include('grades.urls')),
     path('news/', include('news.urls')),
-    # path('api/timetable', include('timetable.api_urls')),
-    # path('api/news/', include('news.api_urls')),
-    # path('api/minimum/', include('minimum.api_urls')),
-    # path('api/diary/', include('diary.api_urls')),
     path('api/', include('api.urls', namespace="api")),
 ]
