354f426ec24fcc112074b6cd1e2e2895cacf578e
Pashs-ba <pashs.ba@gmail.com>
2020-08-18 15:08:04 +0300

    fix bug with mark's deleting

--- school_diary/api/ajax/views.py
+++ school_diary/api/ajax/views.py
@@ -7,13 +7,18 @@ from diary.models import Marks, Students, Lessons
 class SaveMark(APIView):
     def post(self, request):
         data = request.POST
+        print(data['name'])
         student_lesson_ids = data['name'][5:].split('|')
         student_id, lesson_id = int(student_lesson_ids[0]), int(student_lesson_ids[1])
-        value = int(data['value'])
         student = Students.objects.get(pk=student_id)
         lesson = Lessons.objects.get(pk=lesson_id)
         subject = lesson.subject
-        mark = Marks.objects.get_or_create(student=student, lesson=lesson, subject=subject)[0]
-        mark.amount = value
-        mark.save()
+        if data['value']:
+            value = int(data['value'])
+            mark = Marks.objects.get_or_create(student=student, lesson=lesson, subject=subject)[0]
+            mark.amount = value
+            mark.save()
+        else:
+            mark = Marks.objects.get_or_create(student=student, lesson=lesson, subject=subject)[0]
+            mark.delete()
         return Response(status=status.HTTP_200_OK)
